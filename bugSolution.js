abbeb4328b7fafc041b7cec523035f168c8fe2fa
BugHunter-X <askzenai@gmail.com>
2025-02-16 19:10:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="bg-gradient-to-r from-blue-500 to-purple-500 p-4">
+  This is a div with gradient background
+</div>
+```
+This code is corrected by ensuring both `from` and `to` colors are specified in the gradient definition. This will produce the expected gradient from blue to purple.
